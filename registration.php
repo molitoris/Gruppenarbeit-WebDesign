62b66595ca174efa88ca0dc74b072f13577ac22b
Rafael Müller <rafael.mueller1@gmail.com>
2012-08-19 13:42:13 +0200

    Login & Registration
    Erstes Grundgerüst erstellt für Login. Registration kaum ausgearbeitet.

---
+++ registration.php
@@ -0,0 +1,33 @@
+<?php
+	
+	SESSION_START();
+	
+	//Login überprüfen --> zurückleiten
+	
+	//Datenbank login
+		
+	//alle Ergänzungsfächer aus Datenbank auslesen
+	
+	
+	
+	
+	
+?>
+
+
+<html>
+
+<head>
+
+
+</head>
+
+
+<body>
+	
+	<select></select>
+
+</body>
+
+
+</html>
